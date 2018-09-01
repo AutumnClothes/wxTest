995228ed7c0eab7c8a3f3831e2a812c724561fb5
Lee <1085589471@qq.com>
2018-09-02 00:25:35 +0800

    完成时钟

--- pages/apipagetwo/apipagetwo.js
+++ pages/apipagetwo/apipagetwo.js
@@ -1,80 +1,66 @@
-// pages/frame/frame.js
-//导入路由
-var routes = require('routes')
-var util = require('../../utils/util.js')
-
+// pages/apipagetwo/apipagetwo.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    routers: [],
-    msg:'1'
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      routers: routes.routes
-    })
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    this.setInter()
-    this.interval = setInterval(this.setInter, 100);
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
-  },
-  setInter(){
-    this.setData({
-      msg: util.formatTime(new Date())
-    })}
 
-
+  }
 })
--- pages/frame/routes.js
+++ pages/frame/routes.js
@@ -1,88 +1,9 @@
 var routes = [
   {
-    name: '文件结构',
-    url: '/pages/frame/fileStructure/fileStructure',
-    // icon: '/image/icon/home/yy.svg',
+    name: '时钟',
+    url: '/pages/frame/realtime/realtime',
     code: '01'
-  },
-  {
-    name: '配置',
-    url: '/pages/frame/configure/configure',
-    // icon: '/image/icon/home/yy.svg',
-    code: '02'
-  },
-  {
-    name: '逻辑层',
-    url: '/pages/frame/logiclayer/logiclayer',
-    // icon: '/image/icon/home/yy.svg',
-    code: '03'
-  },
-  {
-    name: '视图层',
-    url: '/pages/frame/viewlayer/viewlayer',
-    // icon: '/image/icon/home/yy.svg',
-    code: '04'
-  },
-  {
-    name: '自定义组件',
-    url: '/pages/frame/custom/custom',
-    // icon: '/image/icon/home/yy.svg',
-    code: '05'
-  },
-  {
-    name: '插件',
-    url: '/pages/frame/pluginUnit/pluginUnit',
-    // icon: '/image/icon/home/yy.svg',
-    code: '06'
-  },
-  {
-    name: '分包加载',
-    url: '/pages/frame/subpackage/subpackage',
-    // icon: '/image/icon/home/yy.svg',
-    code: '07'
-  },
-  {
-    name: '多线程',
-    url: '/pages/frame/multithreading/multithreading',
-    // icon: '/image/icon/home/yy.svg',
-    code: '08'
-  },
-  {
-    name: '基础库',
-    url: '/pages/frame/baseLibrary/baseLibrary',
-    // icon: '/image/icon/home/yy.svg',
-    code: '09'
-  },
-  {
-    name: '兼容',
-    url: '/pages/frame/compatible/compatible',
-    // icon: '/image/icon/home/yy.svg',
-    code: '10'
-  },
-  {
-    name: '运行机制',
-    url: '/pages/frame/operating/operating',
-    // icon: '/image/icon/home/yy.svg',
-    code: '11'
-  },
-  {
-    name: '性能',
-    url: '/pages/frame/performance/performance',
-    // icon: '/image/icon/home/yy.svg',
-    code: '12'
-  },
-  {
-    name: '文件系统',
-    url: '/pages/frame/fileSystem/fileSystem',
-    // icon: '/image/icon/home/yy.svg',
-    code: '13'
-  },
-  {
-    name: '基础库更新日志',
-    url: '/pages/frame/updateLog/updateLog',
-    // icon: '/image/icon/home/yy.svg',
-    code: '14'
-  },
+  }
 ]
 
 module.exports = {
